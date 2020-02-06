3849b0a6f8e095c1e1cc07ab95406069fffa6f46
Pratik Kenaudekar <scorp86@gmail.com>
2020-02-06 15:45:49 +0530

    Add project files.

---
+++ Default.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Test.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 195px;
+        }
+    </style>
+    <link href="Content/style.css" rel="stylesheet" />
+    <script src="Scripts/planet.js"></script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table>
+                <tr>
+                    <td><b>Query ( the actual query string ) to get all people whose name contains "Ray":</b></td>                    
+               </tr>
+                <tr>
+                    <td><p>https://swapi.co/api/people/?search=Ray</p></td>
+                </tr>
+            </table>
+        </div>
+        <div id="characters"></div>
+    </form>
+</body>
+</html>
---
+++ Scripts/planet.js
@@ -0,0 +1,52 @@
+﻿function showPlanets(id) {
+
+    fetch(`https://swapi.co/api/planets/${id}`)
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (json) {
+            console.log("data", json)
+
+            if (!json.name) {
+                return;
+            }
+
+            const name = json.name;
+            const diameter = json.diameter;
+            var surface_water = json.surface_water;  
+
+        //    const html = `
+        //<ul class="character">
+        //  <li class="unhabitable">Name: <a href="${json.url}" target="_blank">${name}</a></li>
+        //  <li class="diameter">Diameter: ${diameter}</li>
+        //  <li class="surface_water">Surface Water: ${surface_water}</li>
+        //</ul>`
+        //document.querySelector("#characters").insertAdjacentHTML('afterbegin', html)
+
+            if (Number(surface_water) < 5 || Number(surface_water) == NaN || surface_water == "unknown")  {
+                const html = `
+        <ul class="character">
+          <li class="unhabitable">Name: <a href="${json.url}" target="_blank">${name}</a></li>
+          <li class="diameter">Diameter: ${diameter}</li>
+          <li class="surface_water">Surface Water: ${surface_water}</li>
+        </ul>`
+                document.querySelector("#characters").insertAdjacentHTML('afterbegin', html)
+            }
+            else {
+                const html = `
+        <ul class="character">
+          <li class="habitable">Name: <a href="${json.url}" target="_blank">${name}</a></li>
+          <li class="diameter">Diameter: ${diameter}</li>
+          <li class="surface_water">Surface Water: ${surface_water}</li>
+        </ul>`
+                document.querySelector("#characters").insertAdjacentHTML('afterbegin', html)
+            }            
+      
+      
+
+    })
+}
+
+for (var i = 1; i <= 50; i++) {
+  showPlanets(i)
+}
